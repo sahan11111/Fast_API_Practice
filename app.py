96d0e420a60708ea0e3b7134f8cdfef1ca21955d
Sahan Takhachhen <takhachhensahan@gmail.com>
2026-01-11 21:44:49 +0545

    how query class work learning

--- app.py
+++ app.py
@@ -16,13 +16,19 @@ app = FastAPI()
 
 
 
-class User(BaseModel):
-    username: str
-    password: str
+# class User(BaseModel):
+#     username: str
+#     password: str
     
-'''Body ma default value pathauna ne milxa Body(username=sahan,password=s123) yo rakh da data lai json ma change garna pardei na'''
+# '''Body ma default value pathauna ne milxa Body(username=sahan,password=s123) yo rakh da data lai json ma change garna pardei na'''
 
-@app.post("/signup")
-async def user_signup(user: User =Body(...)): #using Body to explicitly declare request body and data validation or required field and ... means it's required json data
+# @app.post("/signup")
+# async def user_signup(user: User =Body(...)): #using Body to explicitly declare request body and data validation or required field and ... means it's required json data
     
-    return {"message": "User signed up successfully", "user": user}
+#     return {"message": "User signed up successfully", "user": user}
+
+'''query parameters example using Query get request ma data pathaune http://127.0.0.1:8000/login?username=sahan&password=sahan123'''
+@app.post("/login")
+async def user_login(username = Query(...),password=Query(...)): #using Query to explicitly declare query parameters and data validation or required field and ... means it's required query parameters
+    return f"User successfully logged in with username: {username} and password: {password}"
+    
